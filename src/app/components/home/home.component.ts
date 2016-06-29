c146b0d1c8d05605cbffd2f0c0e93df9b45671f1
infining <nathanjgaul@gmail.com>
2016-06-28 20:49:44 -0700

    Added more material components to example app

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,11 +2,17 @@
 import { Component } from '@angular/core';
 
 // Component
+import { MD_LIST_DIRECTIVES } from '@angular2-material/list'
+import { MD_GRID_LIST_DIRECTIVES } from '@angular2-material/grid-list';
 
 @Component({
   selector: 'home',
   templateUrl: './src/app/components/home/home.component.html',
-  styleUrls: [ './src/app/components/home/home.component.css' ]
+  styleUrls: [ './src/app/components/home/home.component.css' ],
+  directives: [
+                MD_LIST_DIRECTIVES,
+                MD_GRID_LIST_DIRECTIVES
+              ]
 })
 
 export class HomeComponent {
